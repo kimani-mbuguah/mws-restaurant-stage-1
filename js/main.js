1e6500187b9cd76058185e5efec2a793adb3cb15
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-08-16 22:31:34 +0300

    fix: add missing click eventListener for view details button

--- js/main.js
+++ js/main.js
@@ -183,9 +183,9 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
-
+  more.addEventListener('click', () => { window.location.href = DBHelper.urlForRestaurant(restaurant); });
+  li.append(more);
+  
   return li
 }
 
