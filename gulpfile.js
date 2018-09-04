24f44bd3bcc8e680554e9d8008bb94f5dfc39c56
kimani-mbuguah <kimanimbuguah@gmail.com>
2018-09-04 16:20:31 +0300

    use gulp-sass and gulp-autoprefixer to generate proper css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-let gulp = require('gulp');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('styles', function() {
+  gulp.src('sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
+  .pipe(gulp.dest('./css'));
+})
